cb585d0b3e64d7487003dbda057ca38f6b076c82
Jackson Diekmann <jacksondiekmann365@gmail.com>
2025-11-23 17:22:42 -0600

    added basic functunatilty

---
+++ src/script/export.js
@@ -0,0 +1,80 @@
+
+function exportFile() {
+  let mapProperties = [];
+  if(infiniteMap) {
+    let xMin = null;
+    let xMax =  null;
+    let yMin = null;
+    let yMax =  null;
+    for (let tileArray of tileMap) 
+      {
+        const tile = tileArray[1];
+
+        if (tile.x > xMax) {
+          xMax = tile.x
+        }
+        if (tile.x < xMin) {
+          xMin = tile.x
+        }
+
+        if (tile.y > yMax) {
+          yMax = tile.y
+        }
+        if (tile.y < yMin) {
+          yMin = tile.y
+        }
+      }
+      mapProperties = {
+        "height": yMax - yMin, 
+        "width": xMax- xMin
+      };
+  } else {
+    mapProperties = {
+      "height": mapHeight, 
+      "width": mapWidth
+    };
+  }
+
+  let mergedTileMap = [];
+  
+  for (let tileArray of tileMap) 
+  {
+    const tile = tileArray[1];
+    mergedTileMap.push(tile);
+  }
+
+  let output = [];
+  output.push(mapProperties);
+  output.push(mergedTileMap);
+
+  
+
+  const blob = new Blob([JSON.stringify(output)], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "test.json";
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+}
+
+async function loadFile() {
+  const fileInput = document.getElementById('inputFile');
+  let text;
+  text = await fileInput.files[0].text();
+  text = JSON.parse(text);
+  console.log(text[0]);
+
+  for (const value of text) {
+    tileMap.set((value.x + "," + value.y), value);
+  }
+}
+
+function getByValue(map, searchValue) {
+  for (let [key, value] of map.entries()) {
+    if (value === searchValue)
+      return key;
+  }
+}
